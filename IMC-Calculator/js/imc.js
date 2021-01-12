16437be5e27e27331347e8ddf38d411933a4a9ed
Lucas Vieira <74318102+LucasVieiraa@users.noreply.github.com>
2021-01-12 14:11:23 -0300

    Add files via upload

---
+++ IMC-Calculator/js/imc.js
@@ -0,0 +1,39 @@
+;(function(){
+    
+    function calculo(){
+        const nome = document.getElementById('nome').value
+        const altura = document.getElementById('altura').value
+        const peso = document.getElementById('peso').value
+        const resultado = document.getElementById('resultado')
+        
+        if(altura === '' || peso === ''){
+        
+         resultado.textContent = 'Preencha todos os campos'
+            
+        }else{
+            
+            let msg = ''
+            let total = (peso / (altura ** 2)).toFixed(1)
+            
+            if (total < 18.5){
+                msg = 'abaixo do peso.'
+            }else if (total < 25) {
+                msg = 'no peso ideal.'
+            }else if (total < 30){
+                msg = 'levemente acima do peso.'
+            }else if (total < 35){
+                msg = 'com obesidade grau I.'
+            }else if (total < 40){
+                msg = 'com obesidade grau II.'
+            }else {
+                msg = 'com obesidade grau III.'
+            }
+            resultado.textContent = `${nome} seu IMC é ${total} e você está ${msg}`
+        }
+    };
+    
+    const btnCalcular = document.getElementById('calcular')
+    btnCalcular.addEventListener('click', calculo)
+    
+    
+})();
